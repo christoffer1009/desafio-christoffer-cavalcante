63c725f187c34c9ead1275d501cf44ce2b87c81d
Christoffer Cavalcante <christoffercavalcante@gmail.com>
2023-08-11 19:26:08 -0300

    valida método de pagamento

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -71,6 +71,10 @@ class CaixaDaLanchonete {
         return false
     }
 
+    metodoDePagamentoInvalido(metodoDePagamento) {
+        return !this.metodosDePagamento.includes(metodoDePagamento)
+    }
+
 
     calcularValorDaCompra(metodoDePagamento, itens) {
         const itensMap = this.mapearitens(itens)
@@ -79,17 +83,21 @@ class CaixaDaLanchonete {
             return 'Item extra não pode ser pedido sem o principal'
         }
 
+        const metodoInvalido = this.metodoDePagamentoInvalido(metodoDePagamento)
+        if (metodoInvalido) {
+            return 'Forma de pagamento inválida!'
+        }
         let valorCompra = 0
         itens.forEach(item => {
             let itemMap = this.mapearItem(item)
             valorCompra += itemMap.quantidade * this.cardapio.find((i) => i.codigo === itemMap.codigo).valor
         });
 
-        return `R$ ${valorCompra.toFixed(2)}`;
+        return `R$ ${valorCompra.toFixed(2).replace('.', ',')}`;
     }
 
 }
 
-console.log(new CaixaDaLanchonete().calcularValorDaCompra('debito', ['sanduiche,1', 'cafe,1']))
+console.log(new CaixaDaLanchonete().calcularValorDaCompra('credito', ['sanduiche,1', 'cafe,1']))
 
 export { CaixaDaLanchonete };
