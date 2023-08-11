2a35d2419c87465edb39309c14fb9427679a3a98
Christoffer Cavalcante <christoffercavalcante@gmail.com>
2023-08-11 20:39:08 -0300

    adiciona comentários

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -1,4 +1,5 @@
 class CaixaDaLanchonete {
+    // Cardápio com os itens e seus códigos, descrições e valores
     cardapio =
         [
             {
@@ -42,18 +43,21 @@ class CaixaDaLanchonete {
                 valor: 7.50
             }
         ]
-
+    // Métodos de pagamento aceitos
     metodosDePagamento = ['debito', 'credito', 'dinheiro']
 
+    // Função para mapear um item a partir de uma string no formato 'codigo,quantidade'
     mapearItem(codigoQuantidade) {
         const item = codigoQuantidade.split(',')
         return { codigo: item[0], quantidade: parseInt(item[1]) }
     }
 
+    // Função para mapear um array de itens no formato 'codigo,quantidade'
     mapearitens(itens) {
         return itens.map(item => this.mapearItem(item))
     }
 
+    // Verifica se há itens extras sem o item principal correspondente
     contemExtraSemPrincipal(itens) {
 
         if (itens.some(i => i.codigo === 'chantily')) {
@@ -71,26 +75,31 @@ class CaixaDaLanchonete {
         return false
     }
 
+    // Verifica se o método de pagamento é válido
     validaMetodoDePagamento(metodoDePagamento) {
         if (!this.metodosDePagamento.includes(metodoDePagamento)) { return { erro: true, mensagem: 'Forma de pagamento inválida!' } }
 
         return true
     }
 
+    // Validação de itens
     validaItens(itens) {
 
         const itensMap = this.mapearitens(itens)
 
+        // Valida a quantiadde = 0
         if (itensMap.some(i => i.quantidade === 0))
             return { erro: true, mensagem: 'Quantidade inválida!' }
 
+        // Valida carrinho vazio
         if (itensMap.length === 0)
             return { erro: true, mensagem: 'Não há itens no carrinho de compra!' }
 
-
+        // Verifica se há itens extras sem o item principal correspondente
         if (this.contemExtraSemPrincipal(itensMap))
             return { erro: true, mensagem: 'Item extra não pode ser pedido sem o principal' }
 
+        // Verifica se há itens com código inválido
         let codigoInvalido
         itensMap.forEach(i => {
             if (!this.cardapio.some(item => item.codigo === i.codigo)) {
@@ -104,12 +113,11 @@ class CaixaDaLanchonete {
         return true
     }
 
+    // Calcula o valor total da compra com base nos itens e método de pagamento
     calcularValorDaCompra(metodoDePagamento, itens) {
         const itensMap = this.mapearitens(itens)
 
         let valorCompra = 0
-
-
 
         const metodoDePagamentoValido = this.validaMetodoDePagamento(metodoDePagamento)
         if (metodoDePagamentoValido.erro) {
@@ -125,6 +133,7 @@ class CaixaDaLanchonete {
             valorCompra += item.quantidade * this.cardapio.find((i) => i.codigo === item.codigo).valor
         });
 
+        // Aplica desconto ou incremento no valor final com base no metodo de pagamento
         switch (metodoDePagamento) {
             case 'credito':
                 valorCompra *= 1.03
@@ -135,11 +144,10 @@ class CaixaDaLanchonete {
                 break;
         }
 
+        // Formata a saída
         return `R$ ${valorCompra.toFixed(2).replace('.', ',')}`;
     }
 
 }
-
-console.log(new CaixaDaLanchonete().calcularValorDaCompra('credito', ['sanduiche,1', 'queijo,0']))
 
 export { CaixaDaLanchonete };
